b1b6b0f38e82306a5cbb3db08900533ccbafb8d2
travis-mcginley <travis.o.mcginley@gmail.com>
2022-10-16 14:45:13 -0400

    Convert achor tags to links

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
 import getProducts from '../services/products';
 
 const ProductList = () => {
@@ -11,7 +12,7 @@ const ProductList = () => {
   return products?.map(product => {
     return (
       <h1 key={product?.id}>
-        <a href={`/products/${product?.id}`}>{product?.name}</a>
+        <Link to={`/products/${product?.id}`}>{product?.name}</Link>
       </h1>
     );
   });
