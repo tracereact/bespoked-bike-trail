f47e430ee6b9222f8361424fb7a57e571116a10e
travis-mcginley <travis.o.mcginley@gmail.com>
2022-10-16 14:43:45 -0400

    Use routing instead of anchor tags

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import ProductList from './components/ProductList';
 
 const App = () => {
-  return <ProductList />;
+  return (
+    <Routes>
+      <Route path="/" element={<ProductList />} />
+      <Route path="/products/:id" element={null} />
+    </Routes>
+  );
 };
 
 export default App;
